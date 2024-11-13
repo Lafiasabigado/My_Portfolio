5bd842b1a463ec31b6033de04adebde26f29cc11
Lafiasabigado <lafiasabigado97@gmail.com>
2024-11-13 16:33:38 +0100

    about coding

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,12 +2,13 @@
 </script>
 
 <template>
-   <div class="pt-4 grid lg:grid-cols-4 md:grid-cols-6 flex-row justify-center items-center">
-      <div class="flex flex-col bg-violet3 rounded-full m-9">
-         <img src="/images/Me.png" alt="" class="w-50">
-      </div>
-      <div class="flex flex-col justify-center">
-         <h2 class="motion-preset-typewriter-[24]">Hello! I'm Lafia Sabi Gado</h2>
-      </div>
-   </div>
+   <div class="pt-4 grid lg:grid-cols-5 md:grid-cols-6 lg:mx-auto flex-row justify-center items-center">
+  <div class="flex flex-col bg-violet3 rounded-full m-9 space-x-96 md:justify-center md:items-center lg:col-start-1 lg:col-end-3">
+    <img src="/images/Me.png" alt="" class="w-50">
+  </div>
+  <div class="flex flex-col justify-center text-center lg:col-start-3 lg:col-end-6 motion-preset-typewriter-[60]">
+    <h2 class="text-3xl pb-10">Hello! I'm Lafia Sabi Gado</h2>
+    <p>Front end Developper</p>
+  </div>
+</div>
 </template>
